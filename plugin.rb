d8e93ba3799ba12baaaa20f486fc873192f1ba58
Eleanor Cawthon <cawthon.e@gmail.com>
2018-10-23 22:25:22 -0700

    more attempts to reconcile

--- plugin.rb
+++ plugin.rb
@@ -13,7 +13,8 @@ after_initialize do
 
     def build_args
         p = Post.find_by_id(@opts[:post_id])
-      {to: @reply_by_email_address,
+      {
+       to: @reply_by_email_address,
        subject: subject,
        body: body,
        charset: 'UTF-8',
@@ -34,7 +35,7 @@ after_initialize do
       # please, don't send us automatic responses...
       result['X-Auto-Response-Suppress'] = 'All'
 
-      if allow_reply_by_email?
+      if Email::MessageBuilder.allow_reply_by_email?
         result[Email::MessageBuilder::ALLOW_REPLY_BY_EMAIL_HEADER] = true
         if @opts[:private_reply] == true
           result['Reply-To'] = @reply_by_email_address
@@ -44,7 +45,7 @@ after_initialize do
           result['CC'] = @reply_by_email_address
         end
       else
-        result['Reply-To'] = from_value
+        result['Reply-To'] = @from_value
       end
 
       result.merge(Email::MessageBuilder.custom_headers(SiteSetting.email_custom_headers))
