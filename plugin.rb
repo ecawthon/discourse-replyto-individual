3b3fd972c56d1143f4bfcbecdca03c1de7436a7c
Eleanor Cawthon <cawthon.e@gmail.com>
2018-10-23 19:20:08 -0700

    try to use particular reply_by_email_address

--- plugin.rb
+++ plugin.rb
@@ -27,11 +27,11 @@ after_initialize do
       if allow_reply_by_email?
         result[Email::MessageBuilder::ALLOW_REPLY_BY_EMAIL_HEADER] = true
         if @opts[:private_reply] == true
-          result['Reply-To'] = reply_by_email_address
+          result['Reply-To'] = @reply_by_email_address
         else
           p = Post.find_by_id @opts[:post_id]
           result['From'] = "#{p.user.name} <#{p.user.email}>"
-          result['To'] = reply_by_email_address
+          result['To'] = @reply_by_email_address
         end
       else
         result['Reply-To'] = from_value
