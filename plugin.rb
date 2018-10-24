a5ff25eb3920476cc57408f95d6c37a4d871c98e
Eleanor Cawthon <cawthon.e@gmail.com>
2018-10-23 22:22:58 -0700

    try with build args?

--- plugin.rb
+++ plugin.rb
@@ -11,6 +11,16 @@ PLUGIN_NAME ||= "replyto-individual".freeze
 after_initialize do
   Email::MessageBuilder.class_eval do
 
+    def build_args
+        p = Post.find_by_id(@opts[:post_id])
+      {to: @reply_by_email_address,
+       subject: subject,
+       body: body,
+       charset: 'UTF-8',
+       from: "#{p.user.name} <#{p.user.email}>"
+      }
+    end
+
     def header_args
       result = {}
       if @opts[:add_unsubscribe_link]
@@ -30,9 +40,8 @@ after_initialize do
           result['Reply-To'] = @reply_by_email_address
         else
           p = Post.find_by_id @opts[:post_id]
-          result['From'] = "#{p.user.name} <#{p.user.email}>"
-          result['Reply-To'] = result['From']
-          result['To'] = @reply_by_email_address
+          result['Reply-To'] = "#{p.user.name} <#{p.user.email}>"
+          result['CC'] = @reply_by_email_address
         end
       else
         result['Reply-To'] = from_value
